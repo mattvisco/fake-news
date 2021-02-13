f1f5651f9b69e2fef0b1280e15e2beb977d5e8e6
Matt Visco <mattvisco@mac.com>
2021-02-13 12:02:12 +0100

    Updated extension to work with nytimes class, changed model to new one

--- inject.js
+++ inject.js
@@ -1,9 +1,19 @@
 var paywallFound = false;
 var findPaywallInterval;
 
+// NYTIMES Class & ID Names
+var PAYWALLID = "gateway-content"; // Used to query the paywall div
+var PAYWALLBTN = 'css-l33adv'; // Used to style our button
+var PAYWALLOVERLAY = "css-1bd8bfl"; // Used to remove paywall overlay
+var MAINCONTENTAREA = "css-mcm29f"; // Used to reenable scrolling
+var SUBTITLE = "article-summary"; // Used to seed the article
+var PARAGRAPHS = "css-axufdj"; // Used to remove/update paragraphs
+var IMAGES = "css-11cwn6f"; // Used to remove/update images
+var CAPTIONS = "css-17ai7jg"; // Used to remove/update image captions
+
 const model = new rw.HostedModel({
-	url: "https://andreas-matt-test-extra-small.hosted-models.runwayml.cloud/v1/",
-	token: "xpQHoYU0Ig/baEwCj2uKQA==",
+	url: "https://ny-times-articles-1500-steps.hosted-models.runwayml.cloud/v1/",
+	token: "GOROiTsPBIsoIRL66okjUg==",
 });
 
 (function() {
@@ -13,7 +23,7 @@ const model = new rw.HostedModel({
 
 function findPaywall() {
 	if (!paywallFound) {
-		var paywall = document.getElementById("gateway-content");
+		var paywall = document.getElementById(PAYWALLID);
 
 		if (paywall) {
 			clearInterval(findPaywallInterval);
@@ -22,8 +32,10 @@ function findPaywall() {
 			// Create fake news button
 			// TODO: style button
 			var fakeNewsButton = document.createElement('button');
-			fakeNewsButton.id = 'injected-button'
-			fakeNewsButton.className = 'css-1dma08p'; // This isn't doing anything anymore
+			fakeNewsButton.id = 'injected-button';
+			// This isn't doing anything anymore
+			// TODO: find the new class for nytimes button and use it?
+			fakeNewsButton.className = PAYWALLBTN;
 			fakeNewsButton.innerHTML = "Create Fake News";
 			fakeNewsButton.style.backgroundColor = "red";
 			fakeNewsButton.style.alignText = "center";
@@ -36,36 +48,37 @@ function findPaywall() {
 
 function makeFakeNews() {
 	// Do runway stuff
-	var prompt = document.getElementsByTagName("h1")[0].innerHTML +  document.getElementById("article-summary").innerHTML;
+	var prompt = document.getElementsByTagName("h1")[0].innerHTML +  document.getElementById(SUBTITLE).innerHTML;
 	const inputs = {
 	  "prompt": prompt,
 	  "max_characters": 1024,
 	  "top_p": 0.9,
 	  "seed": 1000
 	};
+	// TODO: do this iteratively and fill out some set of the paragraphs
 	model.query(inputs).then(outputs => {
 	  const { generated_text, encountered_end } = outputs;
-		document.getElementsByClassName("css-158dogj")[0].innerHTML = generated_text.split(prompt)[1];
+		document.getElementsByClassName(PARAGRAPHS)[0].innerHTML = generated_text.split(prompt)[1];
 	});
 
 
 	// Get rid of paywall
-	var paywall = document.getElementById("gateway-content");
+	var paywall = document.getElementById(PAYWALLID);
 	paywall.style.display = "none";
-	document.getElementsByClassName("css-1bd8bfl")[0].style.display = "none";
-	document.getElementsByClassName("css-mcm29f")[0].style.overflow = "scroll";
+	document.getElementsByClassName(PAYWALLOVERLAY)[0].style.display = "none";
+	document.getElementsByClassName(MAINCONTENTAREA)[0].style.overflow = "scroll";
 
 	// Empty text from paragraphs
-	var paragraphs = document.getElementsByClassName("css-158dogj");
+	var paragraphs = document.getElementsByClassName(PARAGRAPHS);
 	for(var i = 0; i < paragraphs.length; i++) {
 		paragraphs[i].innerHTML = "";
 	}
 
 	// Remove first image
-	var firstImage = document.getElementsByClassName("css-11cwn6f")[0];
+	var firstImage = document.getElementsByClassName(IMAGES)[0];
 	console.log(firstImage);
 	firstImage.style.display = "none";
-	var caption = document.getElementsByClassName("css-17ai7jg")[0];
+	var caption = document.getElementsByClassName(CAPTIONS)[0];
 	console.log(caption);
 	caption.style.display = "none";
 }
