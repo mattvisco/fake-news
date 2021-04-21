fdb118078fb314632890fababd7750a0d873de92
Matt Visco <mattvisco@mac.com>
2021-04-21 20:37:00 +0200

    added tagline and description to paywall

--- inject.js
+++ inject.js
@@ -1,6 +1,7 @@
 var paywallFound = false;
 var findPaywallInterval;
 var fakeNewsButton;
+var fakeNewsTagline;
 var paywall;
 var generatedTextArray = [];
 
@@ -15,6 +16,8 @@ var SUBTITLE = "article-summary"; // Used to seed the article
 var TEXTCONTAINERS = "StoryBodyCompanionColumn";
 var PARAGRAPHS = "css-axufdj"; // Used to remove/update paragraphs
 var IMAGES = "css-79elbk"; // Used to remove images
+var SUBHEADERSTYLE = "css-w6ymp8";
+var DESCRIPTIONSTYLE = "css-19hdyf3";
 
 var TOTALTEXTGENERATION = 2;
 
@@ -39,15 +42,31 @@ function findPaywall() {
 			// Create fake news button
 			fakeNewsButton = document.createElement('button');
 			fakeNewsButton.id = 'injected-button';
-			// This isn't doing anything anymore
-			// TODO: find the new class for nytimes button and use it?
 			fakeNewsButton.className = PAYWALLBTN;
 			fakeNewsButton.innerHTML = "Go Behind The Paywall";
 			fakeNewsButton.style.backgroundColor = "red";
 			fakeNewsButton.style.alignText = "center";
 			fakeNewsButton.onclick = makeFakeNews;
 
+			fakeNewsTagline = document.createElement('h2');
+			fakeNewsTagline.id = 'injected-tagline';
+			fakeNewsTagline.className = SUBHEADERSTYLE;
+			fakeNewsTagline.innerHTML = "It might be wrong, but at least it's free.";
+			fakeNewsTagline.style.marginBottom = "16px";
+
+
+			fakeNewsDescription = document.createElement('p');
+			fakeNewsDescription.id = 'injected-description';
+			fakeNewsDescription.className = DESCRIPTIONSTYLE;
+			fakeNewsDescription.innerHTML = "Behind the paywall is an experiment that uses artificial intelligence to dream up what article might be hidden behind the paywall. Read more <a class='css-19hdyf3' href='https://medium.com/' target='_blank'>here.</a>";
+			fakeNewsDescription.style.marginBottom = "80px";
+			fakeNewsDescription.style.width = "600px";
+			fakeNewsDescription.style.textAlign = "center";
+			fakeNewsDescription.style.color = "rgb(102, 102, 102)";
+
 			paywall.insertBefore(fakeNewsButton, paywall.childNodes[0]);
+			paywall.insertBefore(fakeNewsTagline, paywall.childNodes[1]);
+			paywall.insertBefore(fakeNewsDescription, paywall.childNodes[2]);
 		}
 	}
 }
@@ -110,6 +129,8 @@ function addScanner() {
 	document.getElementsByClassName(PAYWALLOVERLAY)[0].appendChild(scannerBar);
 	document.getElementsByClassName(PAYWALLCONTENT)[0].remove();
 	fakeNewsButton.remove();
+	fakeNewsTagline.remove();
+	fakeNewsDescription.remove();
 
 	// Create creating fake news text
 	var processingText = document.createElement('div');
